369620003abf0f745ebd83d42453a13b064e15b3
Isabelle Louie <inlouie20@gmail.com>
2024-01-21 16:08:21 -0800

    Added fade in animation

---
+++ js/transition.js
@@ -0,0 +1,22 @@
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7
+};
+
+function observerCallback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // fade in observed elements that are in view
+            entry.target.classList.replace('fadeOut', 'fadeIn');
+        } else {
+            // fade out observed elements that are not in view
+            entry.target.classList.replace('fadeIn', 'fadeOut');
+        }
+    });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+const fadeElms = document.querySelectorAll('.fade');
+fadeElms.forEach(el => observer.observe(el));
