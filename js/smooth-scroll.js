1156750ca49512110578aab7f4b4d05c780eacf1
Isabelle <152856143+isabellelouie@users.noreply.github.com>
2026-04-22 11:53:17 -0700

    Smooth scroll animation + case study updates

---
+++ js/smooth-scroll.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const lenis = new Lenis({
+    duration: 1.0,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true,
+    smoothTouch: false,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
